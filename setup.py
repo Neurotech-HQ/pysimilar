ff766d20bda8b88e857139593e338b3ad0ee0576
Kalebu <isaackeinstein@gmail.com>
2022-05-19 16:10:07 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup
 # read the contents of your description file
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'description.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "description.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="pysimilar",
-    version="0.2",
-    description='A very light python libary for comparing similarity between text/strings',
+    version="0.3",
+    description="A very light python libary for comparing similarity between text/strings",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/Kalebu/pysimilar',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kalebu/pysimilar",
     download_url="https://github.com/Kalebu/pysimilar/archive/0.2.tar.gz",
     author="Jordan Kalebu",
     author_email="isaackeinstein@gmail.com",
@@ -24,15 +24,12 @@ setup(
         "python-plagiarism-library",
         "natural language processing",
         "NLP libary",
-        "python-tanzania"
+        "python-tanzania",
     ],
-
     install_requires=[
-        'scikit-learn',
+        "scikit-learn",
     ],
-
     python_requires=">=3.6",
-
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
